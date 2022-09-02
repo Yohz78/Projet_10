3286c76b44ec05994b05db0ef6279cde9fb3c77e
LeoGx <lgarrigoux@gmail.com>
2022-09-02 15:45:44 +0200

    Py client deleted.

--- py_client/detail.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-endpoint = "http://localhost:8000/api/projects/5"
-
-get_response = requests.get(
-    endpoint, json={"title": "camarche?", "description": "Oups"}
-)
-print(get_response.json())
--- py_client/update.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-endpoint = "http://localhost:8000/api/projects/29/update/"
-
-data = {"title": "Helljlkhkjh", "description": "Test"}
-
-get_response = requests.put(endpoint, json=data)
-print(get_response.json())
